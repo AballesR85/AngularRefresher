a3dc4bc884db53b0e18dd802cb44e7d212306876
AballesR85 <aballes.raquel@gmail.com>
2020-06-11 13:38:40 +0200

    add service poke

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ɵangular_packages_platform_browser_platform_browser_k } from '@angular/platform-browser';
+import { PokemonsComponent } from './components/pokemons/pokemons.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home', component: HomeComponent
+  },
+  {
+    path: 'pokemons', component: PokemonsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
